389a30556357a3358b1ee376be7596c73933090d
Luca <luca@banfi.me>
2026-03-24 11:48:47 +0100

    .

---
+++ allarme alieni, esercizio shift e mascheratura.c
@@ -0,0 +1,14 @@
+
+void main (void)
+{
+    int const allarme = 11;
+    TRISA=0b11111111;
+    while {1}
+{
+    if ((PORTA & 0b00001111) >allarme) //mascheratura, tolgo gli ultimi 4 bit
+    {
+        PORTAbits.RA7=1;
+    }else
+        PORTAbits.RA7=1;
+}
+}
