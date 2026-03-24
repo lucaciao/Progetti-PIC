f9f2c021787943e538a232af6fe1ed21d4e79dad
Luca <luca@banfi.me>
2026-03-24 12:00:04 +0100

    esercizio quasi finito

--- allarme alieni, esercizio shift e mascheratura.c
+++ allarme alieni, esercizio shift e mascheratura.c
@@ -5,10 +5,12 @@ void main (void)
     TRISA=0b11111111;
     while {1}
 {
-    if ((PORTA & 0b00001111) >allarme) //mascheratura, tolgo gli ultimi 4 bit
+    if (((PORTA & 0b00111100)>>2) >=allarme) //mascheratura, shift di 2 a destra
     {
         PORTAbits.RA7=1;
     }else
         PORTAbits.RA7=1;
 }
 }
+
+>> 5
--- allarme alieni, esercizion mascheratura.c
+++ allarme alieni, esercizion mascheratura.c
@@ -5,7 +5,7 @@ void main (void)
     TRISA=0b11111111;
     while {1}
     {
-        if ((PORTA & 0b00001111) >allarme) //mascheratura, tolgo gli ultimi 4 bit
+        if ((PORTA & 0b00001111) >=allarme) //mascheratura, tolgo gli ultimi 4 bit
         {
             PORTAbits.RA7=1;
         }else
