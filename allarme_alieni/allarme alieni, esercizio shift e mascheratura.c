cfeeebc1ca745656fd40740bf842ab771d06f91f
Luca Banfi <100074841+lucaciao@users.noreply.github.com>
2026-04-08 20:24:24 +0200

    Fix logic in main function for alarm condition

--- allarme_alieni/allarme alieni, esercizio shift e mascheratura.c
+++ allarme_alieni/allarme alieni, esercizio shift e mascheratura.c
@@ -2,15 +2,15 @@
 void main (void)
 {
     int const allarme = 11;
-    TRISA=0b11111111;
+    TRISA=0b01111111;
+    PORTA=0x00;
+    
     while {1}
-{
-    if (((PORTA & 0b00111100)>>2) >=allarme) //mascheratura, shift di 2 a destra
     {
-        PORTAbits.RA7=1;
-    }else
-        PORTAbits.RA7=1;
+        if (((PORTA & 0b00111100)>>2) >=allarme) //mascheratura, shift di 2 a destra
+        {
+            PORTAbits.RA7=1;
+        }else
+            PORTAbits.RA7=0;
+    }
 }
-}
-
->> 5
